955c35a0657002e4fd42bbc9966793b431668c92
Baibhab <baibhabnayak.31@gmail.com>
2024-08-09 12:00:19 +0530

    Carousel Added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Hero from './Hero';
 import WeatherApp from './Weather';
+import Carousel from './Carousel';
+import slide1 from './slide1.jpg';
+import slide2 from './slide2.jpg';
+import slide3 from './slide3.jpg';
+const images = [
+    slide1,slide2,slide3
+   
+     ];
 const Home = () => {
     return (<div>
          <Hero/>
          <WeatherApp/>
+         <Carousel images={images}/>
     </div>
       
 
